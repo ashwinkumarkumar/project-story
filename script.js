2d1ba63109f691ef35e908076c7b665b4edaa1b2
ashwinkumarkumar <98597432+ashwinkumarkumar@users.noreply.github.com>
2025-02-07 09:34:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript for blog functionality
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Blog page loaded');
+    // Additional JavaScript functionality can be added here
+});
